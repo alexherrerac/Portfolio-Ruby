59614b9ff67bcc8c8c3229049383f023884852aa
Alex <alexxherrerac@gmail.com>
2022-04-14 15:08:08 -0500

    creacion de controlador por terminal

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/contact'
+  get 'pages/about'
   resources :blogs
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
