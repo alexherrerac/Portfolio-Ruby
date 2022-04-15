097af6ac182ba510a92df4e5e35bb42d21c117f7
Alex <alexxherrerac@gmail.com>
2022-04-14 21:31:49 -0500

    Add function to edit portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -17,5 +17,21 @@ class PortfoliosController < ApplicationController
             format.html { render :new, status: :unprocessable_entity }
           end
         end
-      end
+    end
+
+    def edit
+        @portfolio = Portfolio.find(params[:id])
+    end
+
+    def update
+        @portfolio = Portfolio.find(params[:id])
+
+        respond_to do |format|
+          if @portfolio.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+            format.html { redirect_to portfolios_path, notice: "portfolio was successfully updated." }
+          else
+            format.html { render :edit, status: :unprocessable_entity }
+          end
+        end
+    end
 end
