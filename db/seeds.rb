72e7efaac06afcb186311f181df547fe8a523eb6
Alex <alexxherrerac@gmail.com>
2022-04-17 20:52:28 -0500

    implementing scope in the model (filter model)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,23 @@
+3.times do |topic|
+    Topic.create!(
+        title: "Topic #{topic}"
+    )
+end
+
+puts "3 Topics created"
+
 10.times do |x|
-    Blog.create(
+    Blog.create!(
         title: "My Blog  post #{x}", 
-        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        topic_id: Topic.last.id
     )
 end
 
 puts "10 blog posts created"
 
 5.times do |x|
-    Skill.create(
+    Skill.create!(
         title: "Skill #{x}",
         percent_utilized: x * 10
     )
@@ -16,10 +25,20 @@ end
 
 puts "5 skills created"
 
-9.times do |x|
-    Portfolio.create(
+8.times do |x|
+    Portfolio.create!(
         title: "Portfolio title: #{x}",
         subtitle: "Ruby on Rails",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        main_image: "http://placehold.it/600x400",
+        thumb_image: "http://placehold.it/350x200"
+    )
+end
+
+1.times do |x|
+    Portfolio.create!(
+        title: "Portfolio title: #{x}",
+        subtitle: "Angular",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         main_image: "http://placehold.it/600x400",
         thumb_image: "http://placehold.it/350x200"
