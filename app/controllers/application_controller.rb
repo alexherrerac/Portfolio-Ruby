fdbe038aa363acfe99425256534389e24014770e
Alex <alexxherrerac@gamil.com>
2022-04-20 17:58:13 +0000

    added session tracker for dinamic source

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
-    # include DeviseWhiteList
 
+    # include DeviseWhiteList
     before_action :configure_permitted_parameters, if: :devise_controller?
     
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
         devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
+    ########
+
+    before_action :set_source
+
+    def set_source
+        session[:source] = params[:q] if params[:q]
     end
 end
