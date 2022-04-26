e54eb4454fef04bd58373a6e70cdaeb1ab6875c9
Alex <alexxherrerac@gamil.com>
2022-04-26 23:46:51 +0000

    implemented authorization in portfolios and blogs

--- app/controllers/concerns/current_user.rb
+++ app/controllers/concerns/current_user.rb
@@ -6,6 +6,11 @@ module CurrentUserConcern
     end
 
     def guest_user
-        OpenStruct.new(name: "Guest User")
+        guest = GuestUser.new
+        guest.name = "Guest User"
+        guest.first_name = "Guest"
+        guest.last_name = "User"
+        guest.email = "guest@example.com"
+        guest
     end
 end
